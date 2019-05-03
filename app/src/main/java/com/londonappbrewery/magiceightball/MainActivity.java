108ce7c4f2e649554830d61e38b87ab7e54b1ab1
barry <sedatcakir@gmail.com>
2019-05-03 14:37:59 +0300

    Poject complate!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.askButton);
+        final ImageView imageView = (ImageView)findViewById(R.id.image_eightBall);
+        final int[] dizi = {R.drawable.ball1,R.drawable.ball2,
+                R.drawable.ball3,R.drawable.ball4,R.drawable.ball5};
+        final Random randomNumber = new Random();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int number = randomNumber.nextInt(5);
+                imageView.setImageResource(dizi[number]);
+            }
+        });
     }
 }
